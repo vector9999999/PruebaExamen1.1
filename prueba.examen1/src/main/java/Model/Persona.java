99743f8c13a0d361c983e26fb4ddb7603dc6fe03
Victor Narazas <victornarazas2005@gmail.com>
2025-06-02 21:55:03 -0500

    Proyecto iniciado Narazas,Victor

---
+++ prueba.examen1/src/main/java/Model/Persona.java
@@ -0,0 +1,14 @@
+package Model;
+
+public class Persona {
+
+	private int edad = 0;
+	private String nombre = "";
+	
+	
+	public String Diseño() {
+		this.nombre = "Juan";
+		return nombre;
+		
+	}
+}
